f82e9aabe70a044ca497d64eace1d80047679cb4
Rajadurai <rajadurai.s@10decoders.in>
2024-04-26 17:43:02 +0530

    Initial commit

--- TrainTicketBookingMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TrainTicketBookingMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
